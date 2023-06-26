f8599895263b9e4becf0b367faa2825ff5aa60d2
Georgiy <th4tsjake@hotmail.com>
2023-06-27 02:21:26 +0500

    change prod realmeye auth uri; add auth uri to nginx conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: true,
   apiUrl: "https://equiple.net/api",
   authResultType: 'Cookie' as TokenAuthenticationResultType,
-  realmeyeAuthUri: "https://login.equiple.net",
+  realmeyeAuthUri: "https://equiple.net/login",
   fingerprint: "LoXHww7dPTMfRm2iFsJI"
 };
